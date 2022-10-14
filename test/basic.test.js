23c404891df3c814b8a4ea91be17531ef44b05de
Max Daunarovich <max.daunarovich@gmail.com>
2022-10-14 18:59:04 +0300

    Update tests and config

--- test/basic.test.js
+++ test/basic.test.js
@@ -1,5 +1,7 @@
 describe("first", () => {
   it("shall do some magic", () => {
+    console.log("PORT:", process.env.__EMULATOR_PORT__);
+    console.log({ emulatorHash });
     expect(true).toBe(true);
   });
 });
